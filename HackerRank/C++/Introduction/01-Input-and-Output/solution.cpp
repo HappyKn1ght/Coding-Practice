32bc763cb815726aaad6ee9ba351cf94bea54a1a
Yashwant Kumar <sahratan144@gmail.com>
2026-09-12 16:09:25 +0530

    C++ Input and Output

---
+++ HackerRank/C++/Introduction/01-Input-and-Output/solution.cpp
@@ -0,0 +1,19 @@
+/*
+Task
+- Read 3 numbers from stdin and print their sum to stdout.
+- One line that contains  space-separated integers: a, b, and c.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+       int a, b, c;
+       cin >> a >> b >> c;
+       int d = a+b+c;
+       cout << d << endl;
+        
+    return 0;
+}
